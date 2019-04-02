a9734921e6487a870c697957aa3b9ccf4ed50219
PetejStone <petejstoney@gmail.com>
2019-04-02 15:59:36 -0700

    finsihed closure challenges

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -92,7 +92,3 @@ function removeDuplicates(array, cb) {
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
 }
-
-function removeDuplicates(array, cb) {
-  
-}
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,21 +1,60 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function grandparent() {
 
+  const grandpa = "Grandpa Richard I";
+  console.log(`My name is ${grandpa}`);
+  //debugger;
 
-/* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
+  function father() {
+    const father = "Richard II";
+    console.log(`My name is ${father}, my father is ${grandpa}`);
+  //  debugger;
+
+    function grandson() {
+      const grandson = "Richard III"
+      console.log(`My name is ${grandson}, my father is ${father}, and my grandfather is ${grandpa}`);
+    //  debugger;
+    }// town
+
+    grandson();
+  }// city
+
+  father();
+}// kingdom
+
+console.log(grandparent());
+/* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks
+for today's project files */
 
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  // Example usage: const newCounter = counter();
+  // newCounter(); // 1
+  // newCounter(); // 2
+  let count = 0;
+  return () => ++count;
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+   let count = 0;
+   return  {
+     increment: ()=> ++count,
+     decrement:()=> --count
+   }
 };
+const newCounterFactory = counterFactory();
+console.log(newCounterFactory.increment());
+console.log(newCounterFactory.decrement());
+console.log(newCounterFactory.decrement());
